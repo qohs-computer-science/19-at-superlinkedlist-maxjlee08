5b6682798b7b21a49ab7014edab43ca12067317b
maxjlee08 <maxjlee08@gmail.com>
2025-12-04 03:52:49 +0000

    Check in 2

--- SuperLinkedList.java
+++ SuperLinkedList.java
@@ -31,8 +31,24 @@ public class SuperLinkedList extends LinkedList<String> {
     }
 
     public LinkedList<String> removeDuplicates() {
-        return null;
-    }
+        LinkedList<String> dupe = new LinkedList<>();
+        Iterator<String> iter = this.iterator();
+        int index = 0;
+        while (iter.hasNext()){
+            String current = iter.next();
+                for(int i = index + 1; i < this.size(); i++){
+                    if(this.get(i).equals(current)){
+                        dupe.add(this.get(i));
+                        this.remove(i);
+                        i--;
+                    }
+                }
+                index++;
+            }
+            return dupe;
+        }
+       
+    
 
     public LinkedList<String> concatenateStrings() {
 
